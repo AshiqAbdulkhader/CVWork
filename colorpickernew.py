2fc607f4e91c5fe10449d68ff903289b5928dcac
SuhailAhmedVelorum <61581649+SuhailAhmedVelorum@users.noreply.github.com>
2020-10-11 19:53:17 +0530

    Removed unnecessary commented code

--- colorpickernew.py
+++ colorpickernew.py
@@ -8,32 +8,19 @@ pixel = (0,0,0)
 
 def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        # pixelhsv = frame_hsv[y,x]
         pixelbgr = frame[y,x]
-        # pixelbw = frame_bw[y,x]
         print("At (x,y):","("+str(x)+","+str(y)+")")
-        # print("H:",pixelhsv[0],"S:",pixelhsv[1],"V:",pixelhsv[2])
         print("B:",pixelbgr[0],"G:",pixelbgr[1],"R:",pixelbgr[2])
-        # print("B:",pixelbw[0],"W:",pixelbw[1])
-        # print(pixelbw)
         print("__________________________________________________")
 
 val = input("Choose between using your webcam or a locally stored image(webcam/image):")
+cap = cv2.VideoCapture(0)
+
 if(val.lower() == "cam" or val.lower() == "webcam"):
-    val2 = int(input("Have multiple cameras? choose one: "))
-    if(val2 == ""):
-        val2 = 0
     while(1):
-        cap = cv2.VideoCapture(0)
         _,frame = cap.read()
         cv2.imshow("Feed",frame)
-        # frame_bw = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        # cv2.imshow("B/W(Feed)",frame_bw
-        # frame_hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-        # cv2.imshow("HSV(Feed)",frame_hsv)
         cv2.setMouseCallback("Feed", pick_color)
-        # cv2.setMouseCallback("B/W(Feed)", pick_color)
-        # cv2.setMouseCallback("HSV(Feed)", pick_color)
         k = cv2.waitKey(5) & 0xFF
         if(k == 27):
             break
